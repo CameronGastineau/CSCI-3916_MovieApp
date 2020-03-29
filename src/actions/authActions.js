040c92c4b97afa382c2e1ae7a4f07ca053770791
CameronGastineau <CameronGastineau@users.noreply.github.com>
2020-03-29 09:57:49 -0600

    removed an unecessary include of React

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,6 +1,5 @@
 import actionTypes from '../constants/actionTypes';
 import runtimeEnv from '@mars/heroku-js-runtime-env';
-//import React from "react";
 
 function userLoggedIn(username){
     return {
